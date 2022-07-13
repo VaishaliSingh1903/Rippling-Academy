acaa922dfd2c85db8fdf4449df61fdcbd414d2f8
Vaishali Singh <vaishalisinghbh@gmail.com>
2022-07-13 15:29:19 +0530

    login and signup page created

--- Week5/URL_shortner/accounts/models.py
+++ Week5/URL_shortner/accounts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from mongoengine import connect
 
-# Create your models here.
+
+
+
+
--- Week5/URL_shortner/accounts/views.py
+++ Week5/URL_shortner/accounts/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 
 # Create your views here.
+def home(request):
+    #  return HttpResponse("<h1>Ramesh</h1>")
+    return render(request, 'home.html')
+
+def signup(request):
+    return render(request, 'signup.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+# def logout(request):
+#     return render(request, 'signup.html')
